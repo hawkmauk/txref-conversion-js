59916fb9fb062f9abdc17762d5b58dd0294b95fb
Michael Hawkins <michael@runcrypto.com>
2020-03-11 17:34:43 +0000

    txref testing valid inputs

--- src/txref.js
+++ src/txref.js
@@ -1,16 +1,18 @@
+const MAGIC_MAINNET = 0x03
+const MAGIC_MAINNET_EXT = 0x04
+const MAGIC_TESTNET = 0x06
+const MAGIC_TESTNET_EXT=0x07
 
 /**
  * This class provides functions for Txrefs
  */
 class Txref{
 
+	static CHAIN_MAINNET = 'mainnet'
+	static CHAIN_TESTNET = 'testnet'
+
 	static BECH32_HRP_MAINNET = 'tx'
 	static BECH32_HRP_TESTNET = 'txtest'
-
-	static MAGIC_MAINNET = 0x03
-	static MAGIC_MAINNET_EXT = 0x04
-	static MAGIC_TESTNET = 0x06
-	static MAGIC_TESTNET_EXT=0x07
 
 	/**
 	 * Encode transaction location data into a txref
@@ -30,9 +32,28 @@ class Txref{
 	static encode(chain, blockHeight, blockIndex, utxoIndex){
 		
 		//check for valid chain
-		if(chain != 'mainnet' || chain != 'testnet') throw new Error('Invalid chain')
-		//check for extendedTxref
-		//set the magic number
+		if (chain === Txref.CHAIN_MAINNET || chain === Txref.CHAIN_MAINNET)
+			throw new Error('Invalid chain')
+
+		//check for valid blockHeight
+		if (blockHeight > 0xFFFFFF)
+			throw new Error('Invalid block height')
+
+		//check for valid blockIndex
+		if (blockIndex > 0xFFFFFF)
+			throw new Error('Invalid block index')
+
+		//configure txref based on extendedTxref (with utxoIndex)
+		//configure magic value
+		//configure shortId
+		if(utxoIndex !== undefined){
+			this.magic = (chain === Txref.CHAIN_MAINNET) ? MAGIC_MAINNET_EXT : MAGIC_TESTNET_EXT
+			this.shortId = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]; // 12
+		} else {
+			this.magic = (chain === Txref.CHAIN_MAINNET) ? MAGIC_MAINNET : MAGIC_TESTNET
+			this.shortId = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]; // 9
+		}
+
 		return true
 	}
 }
--- tests/testTxref.js
+++ tests/testTxref.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect
+const assert = require('chai').assert
 const Txref = require('../src/txref')
 
 describe('Txref',() => {
@@ -8,10 +9,34 @@ describe('Txref',() => {
 		it('should throw an error on invalid chain', () => {
 			try{
 				Txref.encode('invalid')
+				return false
 			}catch(e){
 				expect(e.message).to.equal('Invalid chain')
 			}
 		})
-	})
 
+		it('should throw an error on invalid block height', () => {
+			try{
+				Txref.encode(Txref.CHAIN_TESTNET)
+				return false
+			}catch(e){
+				expect(e.message).to.equal('Invalid block height')
+			}
+		})
+
+		it('should throw an error on invalid block index', () => {
+			try{
+				Txref.encode(Txref.CHAIN_TESTNET,1000)
+				return false
+			}catch(e){
+				expect(e.message).to.equal('Invalid block index')
+			}
+		})
+
+		it('should return a testnet txref', () => {
+			const txref = Txref.encode(Txref.CHAIN_TESTNET,1201739,2)
+			expect(txref).to.equal('8kyt-fzzq-qqqq-ase0-d8')
+		})
+
+	})
 })
