9cdb6e9483e62ce31425cedb015d4b2f33c8de2a
Michael Hawkins <michael@runcrypto.com>
2020-03-12 13:41:18 +0000

    All txref tests moved over to testTxref.js

--- tests/testTxref.js
+++ tests/testTxref.js
@@ -2,26 +2,113 @@ const expect = require('chai').expect
 const assert = require('chai').assert
 const Txref = require('../src/txref')
 
-const valid_mainnet_1 = {
+const testData = [
+{
+	name: 'encodes a mainnet tx with block height 0 and pos 0',
 	chain: Txref.CHAIN_MAINNET,
 	blockHeight: 0,
 	blockIndex: 0,
 	txref: 'tx1:rqqq-qqqq-qygr-lgl'
-}
-
-const valid_mainnet_2 = {
+},{
+	name: 'encodes a mainnet tx with block height 1 and pos 0',
 	chain: Txref.CHAIN_MAINNET,
 	blockHeight: 1,
 	blockIndex: 0,
 	txref: 'tx1:rzqq-qqqq-qhlr-5ct'
-}
-
-const valid_mainnet_3 = {
+},{
+	name: 'encodes a mainnet tx with block height 2097151 and pos 1000',
 	chain: Txref.CHAIN_MAINNET,
 	blockHeight: 2097151,
 	blockIndex: 1000,
 	txref: 'tx1:r7ll-lrgl-ql0m-ykh'
+},{
+	name: 'encodes a mainnet tx with block height 2097151 and pos 8191',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 2097151,
+	blockIndex: 8191,
+	txref: 'tx1:r7ll-lrll-8e38-mdl'
+},{
+	name: 'encodes a mainnet tx with block height 2097151 and pos 0',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 2097151,
+	blockIndex: 0,
+	txref: 'tx1:r7ll-lrqq-qw4q-a8c'
+},{
+	name: 'encodes a mainnet tx with block height 0 and pos 8191',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 0,
+	blockIndex: 8191,
+	txref: 'tx1:rqqq-qqll-8nvy-ezc'
+},{
+	name: 'encodes a mainnet tx with block height 467883 and pos 2355',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 467883,
+	blockIndex: 2355,
+	txref: 'tx1:rk63-uqnf-z08h-t4q'
+},{
+	name: 'encodes a mainnet tx with block height 0x1FFFFF and pos 0',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 0x1FFFFF,
+	blockIndex: 0,
+	txref: 'tx1:r7ll-lrqq-qw4q-a8c'
+},{
+	name: 'encodes a mainnet tx with block height 0x71F69 and pos 0x89D',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 0x71F69,
+	blockIndex: 0x89D,
+	txref: 'tx1:rjk0-uqay-z0u3-gl8'
+},{
+	name: 'encodes a mainnet tx with block height 466793 and pos 2205',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 466793,
+	blockIndex: 2205,
+	txref: 'tx1:rjk0-uqay-z0u3-gl8'
+},{
+	name: 'encodes a mainnet tx with block height 0 and pos 0x7FFF',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 0,
+	blockIndex: 0x7FFF,
+	txref: 'tx1:rqqq-qqll-lceg-dfk'
+},{
+	name: 'encodes a mainnet tx with block height 0xFFFFFF and pos 0x7FFF',
+	chain: Txref.CHAIN_MAINNET,
+	blockHeight: 0xFFFFFF,
+	blockIndex: 0x7FFF,
+	txref: 'tx1:r7ll-llll-lte5-das'
+},{
+	name: 'encodes a testnet tx with block height 467883 and pos 2355',
+	chain: Txref.CHAIN_TESTNET,
+	blockHeight: 467883,
+	blockIndex: 2355,
+	txref: 'txtest1:xk63-uqnf-zz0k-3h7'
+},{
+	name: 'encodes a testnet tx with block height 0 and pos 0',
+	chain: Txref.CHAIN_TESTNET,
+	blockHeight: 0,
+	blockIndex: 0,
+	txref: 'txtest1:xqqq-qqqq-qfqz-92p'
+},{
+	name: 'encodes a testnet tx with block height 0xFFFFFF and pos 0x7FFF',
+	chain: Txref.CHAIN_TESTNET,
+	blockHeight: 0xFFFFFF,
+	blockIndex: 0x7FFF,
+	txref: 'txtest1:x7ll-llll-lx34-hlw'
+},{
+	name: 'encodes a testnet tx with block height 1152194 and pos 1',
+	chain: Txref.CHAIN_TESTNET,
+	blockHeight: 1152194,
+	blockIndex: 1,
+	txref: 'txtest1:xyv2-xzpq-q63z-7p4'
 }
+
+
+
+
+
+
+
+
+]
 
 describe('Txref',() => {
 
@@ -54,29 +141,17 @@ describe('Txref',() => {
 			}
 		})
 
-		it('encodes a mainnet tx with block height 0 and pos 0', () => {
-			const txref = Txref.encode(
-				valid_mainnet_1.chain,
-				valid_mainnet_1.blockHeight,
-				valid_mainnet_1.blockIndex)
-			expect(txref).to.equal(valid_mainnet_1.txref)
-		})
-
-		it('encodes a mainnet tx with block height 1 and pos 0', () => {
-			const txref = Txref.encode(
-				valid_mainnet_2.chain,
-				valid_mainnet_2.blockHeight,
-				valid_mainnet_2.blockIndex)
-			expect(txref).to.equal(valid_mainnet_2.txref)
-		})
-
-		it('encodes a mainnet tx with block height 2097151 and pos 1000', () => {
-			const txref = Txref.encode(
-				valid_mainnet_3.chain,
-				valid_mainnet_3.blockHeight,
-				valid_mainnet_3.blockIndex)
-			expect(txref).to.equal(valid_mainnet_3.txref)
+		//Run through the test data
+		testData.forEach((test) => {
+				it(test.name, () => {
+					const txref = Txref.encode(
+						test.chain,
+						test.blockHeight,
+						test.blockIndex)
+					expect(txref).to.equal(test.txref)
+				})
 		})
 
 	})
 })
+
